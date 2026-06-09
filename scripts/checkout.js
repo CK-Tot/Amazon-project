98479bd6fe3b70002f620deea479502f1012efd8
CK-Tot <changkuothtot40@gmail.com>
2026-06-09 13:38:47 -0500

    Feat: delete products from html when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -13,7 +13,7 @@ cart.forEach(cartItem => {
 
 
     cartSummaryHtml += `
-        <div class="cart-item-container">
+        <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
                 Delivery date: Tuesday, June 21
             </div>
@@ -100,7 +100,8 @@ deleteLinkEl.forEach((link, index) => {
 
     link.addEventListener('click', () => {
         const productId = link.dataset.productId;
-        removeFromCart(productId);
-        console.log(cart);
+        const deletedProduct = removeFromCart(productId);
+        const productContianer = document.querySelector(`.js-cart-item-container-${productId}`);
+        productContianer.remove();
     })
 })
